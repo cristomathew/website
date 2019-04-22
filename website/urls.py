53169b2280da66ae3e0e62cc8c5b387a03d8e3b8
cristo mathew <cristomathew@cristos-MacBook-Air.local>
2019-04-22 08:55:24 +0530

    template

--- website/urls.py
+++ website/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from home.views import home_view
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
